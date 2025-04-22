1194441f8ede1231e5c46b8e4fac95dc7b145376
Sumitkumar-12354 <Sumitkumar94364@gmail.com>
2025-04-22 10:23:54 +0530

    Add files via upload
    Storage Account Project

--- storage_account.tf
+++ storage_account.tf
@@ -7,11 +7,21 @@ terraform {
   }
 }
 
+provider "azurerm" {
+    subscription_id = "a62a480a-62d9-4e21-822b-77b73bd248e4"
+  features {}
+}
+
+resource "azurerm_resource_group" "app_grp" {
+  name     = "app-grp"
+  location = "Central India"
+}
+
 resource "azurerm_storage_account" "storage_account" {
-  name                     = "storageaccount011"
-  resource_group_name      = "storage-grp"
-  location                 = "Central India"
+  name                     = "storageaccount01104675"
+  resource_group_name      = azurerm_resource_group.app_grp.name
+  location                 = azurerm_resource_group.app_grp.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
-
 }
+
